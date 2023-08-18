5f0ed530fedcf0db54e0dc18444c3d1f0a04c577
Marina <marina8ww1@gmail.com>
2023-08-18 16:46:43 +0700

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/imlearninggit/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.imlearninggit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlin.properties.Delegates
+
+class MainActivity : AppCompatActivity() {
+    private var rrr =1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
